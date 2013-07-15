475c6a4195a5bbc07a8e5adf9789415130df7f60
Jad <jadinvt@gmail.com>
2013-07-14 22:49:41 -0500

    Combining add/list functionality

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 import subprocess
-from bottle import request, route, post, run
+from bottle import redirect, request, route, post, run
 TODO_PATH = 'bin/todo'
 TODO_CONFIG = 'etc/config.todo'
 TODO_BASE_COMMAND = '%s -d %s' %(TODO_PATH, TODO_CONFIG)
@@ -9,7 +9,11 @@ def list(list_name='task.txt'):
 		
 	list = run_command([TODO_PATH, '-d', TODO_CONFIG, 'list'])
 	list = list.replace("\n","<br/>")
-	html ='<b>List function %s</b><br/>' % list
+	html = list
+    html = 'blah'
+    # <input name="task"     type="text" />
+    # <input type="submit" />
+    # </form>"""
 	return html
 
 
@@ -18,9 +22,9 @@ def task():
 	task = request.forms.get('task')
 	if task:
 		run_command([TODO_PATH, '-d', TODO_CONFIG, 'a', "%s" % task])
-	return "<b>Added function %s</b><br/>" % task
+	redirect('/list')
 
-
+@route('/')
 @route('/hello/:name')
 def index(name='World'):
 
